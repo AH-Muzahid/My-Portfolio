ad93639decec1526675dee800dbff69f8ddbb17d
AH Muzahid <ahmuzahid40@gmail.com>
2026-01-05 19:29:48 +0600

    Configured for vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,19 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.simpleicons.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'grainy-gradients.vercel.app',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
